eff55bb3aad68fdfdb0f265c10ca6b14e08146f0
HP <HP@DESKTOP-3K61EP2>
2022-10-20 18:15:57 +0530

    Hello

--- InetBanking/src/main/java/TestCase/LoginTc_001.java
+++ InetBanking/src/main/java/TestCase/LoginTc_001.java
@@ -1,5 +1,69 @@
 package TestCase;
 
-public class LoginTc_001 {
+import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import TestBase.TestBase;
+import TestObject.PageObject;
+
+
+public class LoginTc_001 extends TestBase
+{
+	
+	@Test(priority=1)
+	public void Login_01()
+	
+	{
+		PageObject po = new PageObject(driver);
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		po.UserID();
+		po.UserIDText();
+		
+		if(po.UserIDText())
+		{
+			Assert.assertTrue(true);
+			System.out.println("UserID Text is verified");
+		}
+		else
+		{
+			Assert.assertTrue(false);
+			System.out.println("UserID Text is not verified");
+		}
+		
+		po.Password();
+		po.PasswordText();
+		
+		if(po.UserIDText())
+		{
+			Assert.assertTrue(true);
+			System.out.println("Password Text is verified");
+		}
+		else
+		{
+			Assert.assertTrue(false);
+			System.out.println("Password Text is not verified");
+		}
+		
+		po.RESETText();
+		if(po.UserIDText())
+		{
+			Assert.assertTrue(true);
+			System.out.println("RESET Text is verified");
+		}
+		else
+		{
+			Assert.assertTrue(false);
+			System.out.println("RESET Text is not verified");
+		}		
+		
+		po.LOGIN();
+		
+	}
+	
 }
+				
